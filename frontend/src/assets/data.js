c70a25fe062c665eca8a0c0603432923dcba5c71
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 10:21:28 +0700

    update img to views

---
+++ frontend/src/assets/data.js
@@ -0,0 +1,18 @@
+export default {
+    products: [ // Create properties of products
+        {
+            _id: '1',
+            name: 'White Sports T-Shirt',
+            category: 'T-Shirt',
+            image: '/images/product-1.jpg',
+            price: 150.000,
+            brand: 'Oxygen Sports',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+        },
+        {
+            
+        }
+    ]
+}
