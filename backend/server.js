a5b38e7219208143271e27df33f3dfc2ea8de4b9
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 20:50:05 +0700

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 //Change ES5 Form to ES6 Form: const express = require('express') => import express from 'express'
-import express from 'express';
-import cors from 'cors';
-import data from './data.js';
+import express from "express";
+import cors from "cors";
+import data from "./data.js";
 
-const app1 = express();
+const app = express();
 app.use(cors());
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
     res.send(data.products);
-})
+});
 
 app.listen(5000, () => {
     console.log("Server at http://localhost:5000");
-})
+});
