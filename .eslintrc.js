c030545546111df32c561d9aa0041a6702f6911e
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 21:03:28 +0700

    install some eslint config prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
     env: {
         browser: true,
         node: true,
-        es2023: true,
+        es2020: true,
     },
     extends: ['airbnb-base'], //a list of coding rules that make the code consistent and integrated
     parserOptions: {
         sourceType: 'module',
-        ecmaVersion: 2023,
+        ecmaVersion: 11,
     },
     rules:{
         'no-console': 0,
+        'no-underscore-dangle': 0,
     },
-}
+};
