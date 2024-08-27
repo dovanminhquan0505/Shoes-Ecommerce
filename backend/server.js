a33edb175447bef70271c315004ee3308ac9c367
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 16:27:54 +0700

    create api products

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const data = require('./data.js')
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    
+    res.send(data.products);
+})
+
+app.listen(5000, () => {
+    console.log("Server at http://localhost:5000");
 })
