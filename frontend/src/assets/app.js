2b37073672c23a5a246d5801928f29e8b835fddc
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 11:28:08 +0700

    add some notes

--- frontend/src/assets/app.js
+++ frontend/src/assets/app.js
@@ -16,7 +16,9 @@ const router = () => {
         (request.verb ? `/${request.verb}` : '');
     const screen = routes[parseUrl]? routes[parseUrl]: Error404Screen;
     const main = document.getElementById('main-container');
-    main.innerHTML = screen.render();
+    main.innerHTML = screen.render(); //render the products to views
 };
+
+//add event listener to the window when the page load and when the hash change
 window.addEventListener("load", router);
 window.addEventListener('hashchange', router);
