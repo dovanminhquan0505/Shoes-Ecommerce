fa8b9690c35e77b6fb5d8ce0af1d743ceb070b68
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 18:43:40 +0700

    change to use axios package, instead of using fetch

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,19 @@
+import axios from 'axios';
 const homeScreen = {
     //Code to get data from frontend to backend
     render: async () => {
         // Code to fetch data from backend using fetch API
-        const response = await fetch("http://localhost:5000/api/products", {
+        const response = await axios({ //change fetch to axios when installing npm axios package
+            url: "http://localhost:5000/api/products", 
             headers: {
                 'Content-Type': 'application/json',
             }
         });
-        if(!response || !response.ok){
+        if(!response || response.statusText !== 'OK'){
             return `<div>Error in getting data</div>`;
         };
 
-        const products = await response.json();
+        const products = response.data;
 
         return `
             <ul class="products">
