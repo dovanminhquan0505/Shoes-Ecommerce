5b184cddcea6ac49bf2046ca649088b88517299b
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-27 10:44:10 +0700

    create HomeScreen JS and create HTML Form to render to views

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import data from '../assets/data.js'
+const homeScreen = {
+    render: () => {
+        const { products } = data; 
+        return `
+            <ul class="products">
+                ${products.map(product => `
+                    <li>
+                        <div class="product">
+                            <a href="/#/product/${product._id}">
+                                <img src="${product.image}" alt="${product.name}" />
+                            </a>
+                            <div class="product-name">
+                                <a href="/#/product/${product._id}">
+                                    ${product.name}
+                                </a>
+                            </div>
+                            <div class="product-brand">
+                                ${product.brand}
+                            </div>
+                            <div class="product-price">
+                                ${product.price}$
+                            </div>
+                        </div>
+                    </li>    
+                `)}
+            </ul>
+        `
+    }
+}
