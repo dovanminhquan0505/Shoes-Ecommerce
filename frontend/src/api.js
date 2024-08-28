493aec6dde7102ee91e8ea1ea64c4ed5b436c2a8
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-28 15:57:15 +0700

    complete api js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { apiUrl } from "./config";
 
 export const getProduct = async (id) => {
@@ -9,7 +10,13 @@ export const getProduct = async (id) => {
                 'Content-Type': 'application/json',
             }
         });
+        //when can't get API response, web will return error message
+        if(response.statusText !== 'OK'){ 
+            throw new Error(response.data.message); 
+        }
+        return response.data;
     } catch (error) {
-        
+        console.log(error);
+        return { error: error.message };
     }
 };
