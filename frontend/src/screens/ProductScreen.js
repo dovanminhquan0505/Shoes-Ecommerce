a1b88b95819cf84bc7a418b28f491c8ff6c91dce
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-28 15:45:54 +0700

    create request to the server

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,9 @@
+import { parseRequestUrl } from '../utils.js'
 
 const ProductScreen = {
-    render: () => {
-        return (
-            `<div>ProductScreen</div>`
-        )
+    render: async () => {
+        const request = parseRequestUrl();
+        const product = await getProduct(request.id);
     }
 }
 
