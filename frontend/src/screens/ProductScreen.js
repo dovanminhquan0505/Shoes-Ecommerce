5bb719994f924bf0baa2744bf38086d7d93b3229
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-28 19:19:18 +0700

    create format product views

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
 import { getProduct } from '../api.js';
+import Rating from '../components/Rating.js';
 import { parseRequestUrl } from '../utils'
 
 const ProductScreen = {
@@ -8,7 +9,54 @@ const ProductScreen = {
         if(product.error){
             return (`<div>${product.error}</div>`)
         }
-        return `<h1>${product.name}</h1>`
+        return `
+            <div class="content">
+                <div class="back-to-result">
+                    <a href="/#/">Back to result</a>
+                </div>
+                <div class="details">
+                    <div class="details-image">
+                        <img src="${product.image}" alt="${product.name}" />
+                    </div>
+                    <div class="details-info">
+                        <ul>
+                            <li>
+                                <h1>${product.name}</h1>
+                            </li>
+                            <li>
+                                ${Rating.render({
+                                    value: product.rating,
+                                    text: `${product.numReviews} reviews`,
+                                })}
+                            </li>
+                            <li>
+                                Price: <strong>${product.price}$</strong>
+                            </li>
+                            <li>
+                                Description: 
+                                <div>
+                                    ${product.description}
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="details-action">
+                        <ul>
+                            <li>
+                                Price: ${product.price}$
+                            </li>
+                            <li>
+                                Status: 
+                                    ${product.countInStock > 0 ? `<span class="success">In Stock</span>` : `<span class="fail">Out of Stock</span>`}
+                            </li>
+                            <li>
+                                <button id="add-button" class="primary">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        `
     },
 }
 
