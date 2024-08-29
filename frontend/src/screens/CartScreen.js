8b15ba8a5410b87491e7679d96a612f8930d3834
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-29 18:35:27 +0700

    create addToCart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,30 @@
+import { getProduct } from "../api";
+import { parseRequestUrl } from "../utils";
+
+const addToCart = (item, forceUpdate = false) => {
+    let cartItems = getCartItems();
+}
+
 const CartScreen = {
     after_render: () => {
 
     },
-    render: () => {
+    render: async () => {
+        const response = parseRequestUrl();
+        if(response.id){
+            //Users click on add to cart
+            const product = await getProduct(response.id);
+            addToCart({
+                product: product._id,
+                name: product.name,
+                image: product.image,
+                price: product.price,
+                countInStock: product.countInStock,
+                quantity: 1,
+            })
+        } else {
+            //Users click on the cart in the menu
+        }
         return `<div>Cart Screen</div>`;
     }
 }
