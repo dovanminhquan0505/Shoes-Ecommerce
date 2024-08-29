10d74e6f8cd49768a14e28df91283d1aa3c37cdf
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-29 18:45:35 +0700

    create condition of addToCart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,20 @@
 import { getProduct } from "../api";
+import { getCartItems } from "../localStorage";
 import { parseRequestUrl } from "../utils";
 
 const addToCart = (item, forceUpdate = false) => {
     let cartItems = getCartItems();
+    //x.product is the ID of items inside cart of items 
+    const existItem = cartItems.find(x => x.product === item.product);
+    if(existItem){
+        cartItems = cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+        );
+    } else {
+        cartItems = [...cartItems, item];
+    }
+    //Update the cart items
+    setCartItems(cartItems);
 }
 
 const CartScreen = {
