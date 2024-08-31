39ae9f49dbe49a341d59ec06620c150fb63d7870
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-31 16:38:00 +0700

    create showMessage function

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -20,3 +20,19 @@ export const showLoading = () => {
 export const hideLoading = () => {
     document.getElementById('loading-overlay').classList.remove('active');
 }
+
+export const showMessage = (message, callback) => {
+    document.getElementById('message-overlay').innerHTML = `
+        <div>
+            <div id="message-overlay-content">${message}</div>
+            <button id="message-overlay-close-button">OK</button>
+        </div>
+    `;
+    document.getElementById('message-overlay').classList.add('active');
+    document.getElementById('message-overlay-close-button').addEventListener('click', () => {
+        document.getElementById('message-overlay').classList.remove('active');
+        if(callback){
+            callback();
+        }
+    })
+}
