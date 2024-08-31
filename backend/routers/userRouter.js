5417af0c78366846a1cf8651bb3bd3f71041d94d
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-31 14:21:04 +0700

    create POST request

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -18,4 +18,16 @@ userRouter.get("/createadmin", async (req, res) => {
     }
 });
 
+//Add new API and Post API requests
+userRouter.post('/signin', async (req, res) => {
+    const signinUser = await User.findOne({
+        //Have to install body-parser
+        email: req.body.email,
+        password: req.body.password,
+    });
+    if(!signinUser){
+        res.status(401).send({message: 'Invalid email or password!'})
+    }
+})
+
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import data from "./data.js";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import config from "./config.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -20,6 +21,7 @@ mongoose
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
 app.use("/api/users", userRouter);
 app.get("/api/products", (req, res) => {
     res.send(data.products);
