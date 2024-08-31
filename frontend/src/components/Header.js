25304ca02d07d64a883fa97985d3b24c9305ae15
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-08-31 15:56:02 +0700

    fix header, if name exists, show the name of user to views and avoid signin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
+import { getUserInfo } from "../localStorage";
+
 const Header = {
     render: () => {
+        const {name} = getUserInfo();
         return `
             <div class="brand">
                 <a href="/#/">Shoes Haven</a>
             </div>
             <div>
+            ${name 
+                ? `<a href="/#/profile">${name}</a>` 
+                : `<a href="/#/signin">Sign-In</a>`
+            }
                 <a href="/#/cart">Cart</a>
-                <a href="/#/signin">Sign-In</a>
             </div>
         `
     },
