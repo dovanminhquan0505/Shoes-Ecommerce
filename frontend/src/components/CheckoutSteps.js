2a87f27bb468fe90b0a8ee283dddb3433a9bc570
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-01 16:42:57 +0700

    fix bug for style and name of form

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -5,7 +5,7 @@ const CheckoutSteps = {
                 <div class="${props.step1 ? 'active' : ''}">SignIn</div>
                 <div class="${props.step2 ? 'active' : ''}">Shipping</div>
                 <div class="${props.step3 ? 'active' : ''}">Payment</div>
-                <div class="${props.step4 ? 'active' : ''}">PlaceOrder</div>
+                <div class="${props.step4 ? 'active' : ''}">Place Order</div>
             </div>
         `
     }
