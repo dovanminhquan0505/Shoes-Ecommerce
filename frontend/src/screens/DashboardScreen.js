0bc56951059ee9b304eb071a1225ed47175011fc
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-02 22:45:20 +0700

    add the dashboardmenu into screen

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,9 +1,21 @@
+import DashboardMenu from "../components/DashboardMenu";
+
 const DashboardScreen = {
     after_render: () => {
         
     },
     render: () => {
-        return "Dashboard";
+        return `
+            <div class="dashboard">
+                ${DashboardMenu.render({selected:'dashboard'})}
+                <div class="dashboard-content">
+                    <h1>Dashboard</h1>
+                    <div>
+                        Info and Charts will be added here
+                    </div>
+                </div>
+            </div>
+        `;
     },
 }
 
