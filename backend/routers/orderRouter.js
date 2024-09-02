5e02cc8659984b6db1e7e504477b946558ae16f9
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-02 14:09:55 +0700

    complete orderRouter

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -4,6 +4,14 @@ import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel";
 
 const orderRouter = express.Router();
+orderRouter.get('/:id', isAuth, expressAsyncHandler( async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if(order){
+        res.send(order);
+    } else {
+        res.status(404).send({message: 'Order not found'});
+    }
+}));
 
 //Create a route for building or creating an order
 orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
