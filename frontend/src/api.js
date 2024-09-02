3e3b159005d5a6b469188c2df306c1b798a05207
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-02 13:57:23 +0700

    create getOrder function

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -118,3 +118,23 @@ export const createOrder = async (order) => {
         return {error: (error.response ? error.response.data.message : error.message)};
     }
 }
+
+export const getOrders = async (id) => {
+    try {
+        const {token} = getUserInfo();
+        const response = await axios({
+            url: `${apiUrl}/api/orders/${id}`,
+            method: 'GET',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        if(response.statusText !== 'OK'){
+            throw new Error(response.data.message);
+        }
+        return response.data;
+    } catch (error) {
+        return {error: error.message};
+    }
+}
