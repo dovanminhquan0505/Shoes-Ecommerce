8dd0543cdebb1fc934e6478f94b5e209e4e2daa4
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-02 16:21:56 +0700

    update new strategy and create get orderRouter to mine profile

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -4,6 +4,12 @@ import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel";
 
 const orderRouter = express.Router();
+
+orderRouter.get('/mine', isAuth, expressAsyncHandler(async(req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+}));
+
 orderRouter.get('/:id', isAuth, expressAsyncHandler( async (req, res) => {
     const order = await Order.findById(req.params.id);
     if(order){
