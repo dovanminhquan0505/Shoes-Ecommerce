ea85718a1f9c8640daa34658ba12b8a0a3e5fb3b
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-02 22:29:07 +0700

    create dashboard if user is an admin

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { getUserInfo } from "../localStorage";
 
 const Header = {
     render: () => {
-        const {name} = getUserInfo();
+        const {name, isAdmin} = getUserInfo();
         return `
             <div class="brand">
                 <a href="/#/">Shoes Haven</a>
@@ -13,6 +13,10 @@ const Header = {
                 : `<a href="/#/signin">Sign-In</a>`
             }
                 <a href="/#/cart">Cart</a>
+                ${isAdmin
+                    ? `<a href="/#/dashboard">Dashboard</a>`
+                    : ''
+                 }
             </div>
         `
     },
