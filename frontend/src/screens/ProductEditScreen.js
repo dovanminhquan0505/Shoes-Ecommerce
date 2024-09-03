497dec972f6063df04c4902cb698ec923156093e
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-03 15:15:06 +0700

    complete form edit product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -8,7 +8,53 @@ const ProductEditScreen = {
     render: async () => {
         const request = parseRequestUrl();
         const product = await getProduct(request.id);
-        return `${product.name}`;
+        return `
+            <div class="content">
+                <div>
+                    <a href="/#/productlist">Back to products</a>
+                </div>
+                <div class="form-container">
+                    <form id="edit-product-form">
+                        <ul class="form-items">
+                            <li>
+                                <h1>Edit Product ${product._id.substring(0, 8)}</h1>
+                            </li>
+                            <li>
+                                <label for="name">Name</label>
+                                <input type="text" name="name" id="name" value="${product.name}"/>
+                            </li>
+                            <li>
+                                <label for="price">Price</label>
+                                <input type="number" name="price" id="price" value="${product.price}"/>
+                            </li>
+                            <li>
+                                <label for="image">Image (680 x 830)</label>
+                                <input type="file" name="image" id="image" value="${product.image}"/>
+                            </li>
+                            <li>
+                                <label for="brand">Brand</label>
+                                <input type="text" name="brand" id="brand" value="${product.brand}"/>
+                            </li>
+                            <li>
+                                <label for="countInStock">Count In Stock</label>
+                                <input type="number" name="countInStock" id="countInStock" value="${product.countInStock}"/>
+                            </li>
+                            <li>
+                                <label for="category">Category</label>
+                                <input type="text" name="category" id="category" value="${product.category}"/>
+                            </li>
+                            <li>
+                                <label for="description">Description</label>
+                                <input type="text" name="description" id="description" value="${product.description}"/>
+                            </li>
+                            <li>
+                                <button type="submit" class="primary">Update</button>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        `;
     }
 }
 
