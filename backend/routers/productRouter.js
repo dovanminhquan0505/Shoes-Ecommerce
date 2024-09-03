f8fa673bb87046dec2f400c3e752086620103b6c
Đỗ Văn Minh Quân <minhquandovan@gmail.com>
2024-09-03 15:32:57 +0700

    create update product from backend

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -31,4 +31,26 @@ productRouter.post('/', isAuth, isAdmin, expressAsyncHandler( async (req, res) =
     }
 }));
 
+productRouter.put('/:id', isAuth, isAdmin, expressAsyncHandler( async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if(product){
+        product.name = req.body.name;
+        product.price = req.body.price;
+        product.image = req.body.image;
+        product.brand = req.body.brand;
+        product.category = req.body.category;
+        product.countInStock = req.body.countInStock;
+        product.description = req.body.description;
+        const updatedProduct = await product.save();
+        if(updatedProduct){
+            res.send({ message: 'Product Updated!', product: updatedProduct });
+        } else {
+            res.status(500).send({ message: 'Error in updating product' });
+        }
+    }else {
+        res.status(404).send({ message: 'Product not found' });
+    }
+}))
+
 export default productRouter;
